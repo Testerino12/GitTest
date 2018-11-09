18a03cbc0a3762362dcf0532bd8480f043d905f1
tmj <git@tmj.li>
2018-11-09 12:56:40 +0100

    bla

--- uebung/src/main/java/de/oio/maven/uebung/test/App.java
+++ uebung/src/main/java/de/oio/maven/uebung/test/App.java
@@ -11,6 +11,7 @@ import org.apache.commons.io.FileUtils;
 
 public class App{
     public static void main( String[] args ){
+    	System.out.println("bla");
         File localizedFile = getLocalizedFile();
    
 		try {
